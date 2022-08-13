b12b848d39dfd421b3196f1e647b28bc7af8527b
ParkSeongMoon <psmoon7304@naver.com>
2022-08-13 19:11:06 +0900

    Base V0.0.1

--- main.js
+++ main.js
@@ -1,22 +1,42 @@
 const _admin = ["또바기","박성문","키즈힘"];
 let _players = App.players;
 
-App.showCenterLabel("Hello world!");
-
-function joinMegDelay() {
-    
+function addPlayerMsg(player,text,delay) {
+    player.tag.playerMsgCount += 1;
+    player.tag.playerMsgList.push([text,delay]);
 }
 
 App.onJoinPlayer.Add(function(player) {
     _players = App.players;
+
+    player.moveSpeed = 400;
     player.tag = {
         admin: false,
-        playerDealy: 0,
+        playerMsgCount: 0,
+        playerMsgList: [] // String - 메시지 내용, Int - Dealy 단위 : 1,2,3~ | example : [["Text",0]]
     }
+
     if(_admin.includes(player.name)) {
         player.tag.admin = true;
-        player.tag
-        player.showCenterLabel(`${player.name}님 어서오세요. 현재 계정은 어드민으로 등록되어 있습니다.`);
-        player.sayToAll(`${App.playerCount}명이 현재 접속중입니다.`);
+        player.showCenterLabel(`${player.name}님 어서오세요. 현재 계정은 어드민으로 등록되어 있습니다.`,bgColor=0xFFFF00);
+        addPlayerMsg(player,`${App.playerCount}명이 현재 접속중입니다.`,1.5);
+    }
+    player.sendUpdated();
+});
+
+App.onUpdate.Add(function(d) {
+    for(let i in _players) {
+        let pInfo = _players[i];
+        
+        if(pInfo.tag.playerMsgCount >= 1) { //Client CenterLabel Part
+            if(pInfo.tag.playerMsgList[0][1] <= 0) {
+                pInfo.showCenterLabel(pInfo.tag.playerMsgList[0][0]);
+                pInfo.tag.playerMsgCount -= 1
+                pInfo.tag.playerMsgList.splice(0,1)
+                App.sayToAll(pInfo.tag.playerMsgList.length)
+            } else {
+                pInfo.tag.playerMsgList[0][1] -= d;
+            }
+        }
     }
 });
